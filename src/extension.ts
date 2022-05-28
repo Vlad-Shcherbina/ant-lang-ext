71fbdd6c029eebbd73af0168b23ce8eda1d3f20c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-05-28 18:50:51 +0200

    Inlay hints with state numbers

--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,29 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
     context.subscriptions.push(disp);
+
+    disp = vscode.languages.registerInlayHintsProvider("ant", {
+        provideInlayHints(document, range, token) {
+            console.log(`inlay hints ${range.start.line}:${range.start.character}-${range.end.line}:${range.end.character}`);
+            let hints: vscode.InlayHint[] = [];
+            // TODO: add hint on the last line, but only when it's not empty
+            let w = 1;
+            let n = 10;
+            while (range.end.line - 1 >= n) {
+                w += 1;
+                n *= 10;
+            }
+            for (let line = range.start.line; line < range.end.line; line++) {
+                let hint = new vscode.InlayHint(
+                    new vscode.Position(line, 0), line.toString().padStart(w, ' ') + ':');
+                hint.paddingRight = true;
+                hints.push(hint);
+            }
+            console.log(hints);
+            return hints;
+        }
+    });
+    context.subscriptions.push(disp);
 }
 
 export function deactivate() {
